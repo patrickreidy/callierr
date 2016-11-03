abadc2d06a0002c14085e2d68b46432dad55f78a
Patrick Reidy <patrick.francis.reidy@gmail.com>
2016-11-03 12:38:30 -0500

    Exported constants for the six Callier Center colors

---
+++ R/data.R
@@ -0,0 +1,96 @@
+
+#' Solar Orange
+#'
+#' The hexadecimal code that specifies Pantone 144 orange ('solar orange'),
+#' which corresponds to 233-131-0 in the RGB (red-green-blue) color model.
+#'
+#' Solar orange is one of the three primary Callier Center colors
+#' (solar orange, space blue, callier gray), which should be used when depicting
+#' Callier Center colors in print or electronic formats. Of the primary
+#' Callier Center colors, solar orange has first precedence, space blue has
+#' second, and callier gray has third. Hence, one-color images should use
+#' solar orange; two-color images should use solar orange and space blue; and
+#' three-color images should use solar orange, space blue, and callier gray.
+#'
+#' @aliases #E98300 #e98300
+#' @format A character string, \code{'#E98300'}.
+#' @source Callier Center Graphic Standards Manual
+'solarOrange'
+
+#' Space Blue
+#'
+#' The hexadecimal code that specifies Pantone 286 blue ('space blue'),
+#' which corresponds to 0-57-166 in the RGB (red-green-blue) color model.
+#'
+#' Space blue is one of the three primary Callier Center colors
+#' (solar orange, space blue, callier gray), which should be used when depicting
+#' Callier Center colors in print or electronic formats. Of the primary
+#' Callier Center colors, solar orange has first precedence, space blue has
+#' second, and callier gray has third. Hence, one-color images should use
+#' solar orange; two-color images should use solar orange and space blue; and
+#' three-color images should use solar orange, space blue, and callier gray.
+#'
+#' @aliases #0039A6 #0039a6
+#' @format A character string, \code{'#0039A6'}.
+#' @source Callier Center Graphic Standards Manual
+'spaceBlue'
+
+#' Callier Gray
+#'
+#' The hexadecimal code that specifies Pantone warm gray 10 ('callier gray'),
+#' which corresponds to 118-106-98 in the RGB (red-green-blue) color model.
+#'
+#' Callier gray is one of the three primary Callier Center colors
+#' (solar orange, space blue, callier gray), which should be used when depicting
+#' Callier Center colors in print or electronic formats. Of the primary
+#' Callier Center colors, solar orange has first precedence, space blue has
+#' second, and callier gray has third. Hence, one-color images should use
+#' solar orange; two-color images should use solar orange and space blue; and
+#' three-color images should use solar orange, space blue, and callier gray.
+#'
+#' @aliases #766A62 #766a62
+#' @format A character string, \code{'#766A62'}.
+#' @source Callier Center Graphic Standards Manual
+'callierGray'
+
+#' Spark Orange
+#'
+#' The hexadecimal code that specifies Pantone 1235 orange ('spark orange'),
+#' which corresponds to 255-182-18 in the RGB (red-green-blue) color model.
+#'
+#' Spark orange is one of the three secondary Callier Center colors
+#' (spark orange, stratos blue, sky blue) that are preferred as accents to
+#' complement the primary Callier Center colors in print or electronic formats.
+#'
+#' @aliases #FFB612 #ffb612
+#' @format A character string, \code{'#FFB612'}.
+#' @source Callier Center Graphic Standards Manual
+'sparkOrange'
+
+#' Stratos Blue
+#'
+#' The hexadecimal code that specifies Pantone 299 blue ('stratos blue'),
+#' which corresponds to 0-161-222 in the RGB (red-green-blue) color model.
+#'
+#' Stratos blue is one of the three secondary Callier Center colors
+#' (spark orange, stratos blue, sky blue) that are preferred as accents to
+#' complement the primary Callier Center colors in print or electronic formats.
+#'
+#' @aliases #00A1DE #00a1de
+#' @format A character string, \code{'#00A1DE'}.
+#' @source Callier Center Graphic Standards Manual
+'stratosBlue'
+
+#' Sky Blue
+#'
+#' The hexadecimal code that specifies Pantone 2985 blue ('sky blue'),
+#' which corresponds to 91-198-232 in the RGB (red-green-blue) color model.
+#'
+#' Sky blue is one of the three secondary Callier Center colors
+#' (spark orange, stratos blue, sky blue) that are preferred as accents to
+#' complement the primary Callier Center colors in print or electronic formats.
+#'
+#' @aliases #5BC6EB #5bc6eb
+#' @format A character string, \code{'#5BC6EB'}.
+#' @source Callier Center Graphic Standards Manual
+'skyBlue'
---
+++ data-raw/standards-colors.R
@@ -0,0 +1,16 @@
+
+
+# Primary Callier Center colors
+solarOrange <- '#E98300'
+spaceBlue   <- '#0039A6'
+callierGray <- '#766A62'
+
+# Secondary Callier Center colors
+sparkOrange <- '#FFB612'
+stratosBlue <- '#00A1DE'
+skyBlue     <- '#5BC6EB'
+
+devtools::use_data(
+  solarOrange, spaceBlue, callierGray, sparkOrange, stratosBlue, skyBlue,
+  internal = FALSE, overwrite = FALSE
+)
