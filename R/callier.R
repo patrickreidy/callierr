4ae893e5d4ddc19b7c5af1f098a91a6d6450a573
Patrick Reidy <patrick.francis.reidy@gmail.com>
2016-11-08 15:48:43 -0600

    Documented callier package

---
+++ R/callier.R
@@ -0,0 +1,47 @@
+
+#' callier: Color Palettes and Scales for the Callier Center
+#'
+#' An implementation of the color schemes set out by the Graphic Standards for
+#' the Callier Center for Communication Disorders.
+#'
+#' @section License:
+#' Do not distrubte outside of faculty and staff of the Callier Center
+#' for Communication Disorders.
+#'
+#' @section Constants:
+#' solarOrange
+#'
+#' spaceBlue
+#'
+#' callierGray
+#'
+#' sparkOrange
+#'
+#' stratosBlue
+#'
+#' skyBlue
+#'
+#' @section Color-model converters (internal):
+#' as.hex()
+#'
+#' as.hsl()
+#'
+#' as.rgb()
+#'
+#' @section Palette constructors:
+#' CallierQualitative()
+#'
+#' CallierSequential()
+#'
+#' CallierDiverging()
+#'
+#' @section Scale constructors (for ggplot objects):
+#' scale_color_callier()
+#'
+#' @section Other functions:
+#' ColorChart()
+#'
+#' @docType package
+#' @name callier
+NULL
+
